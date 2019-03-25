8d0404f44048c77020703cc5b2e7de3020af932d
celine28240 <celinehenichard@gmail.com>
2019-03-25 16:52:19 +0100

    Correction quote

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-user-profile',
@@ -6,16 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
+@Input() label: string = "hidden";
+@Output() onclick = new EventEmitter<string>();
+
   user={
     name : "Henichard",
     firstName : "Celine",
     age :42,
     quote:"",
     photo: "https://tof.cx/images/2019/03/25/c4e9a84e07abc7de30a53cc6e852b252.jpg"
-  };
+  }
+
 //rajouter fonction hiddenage
 CacherAge(){
-  document.getElementById("cacher").style.visibility ="hidden";
+  if (this.label == "hidden"){
+    this.label= "visible";
+  }
+  else{
+    this.label="hidden";
+  }
+  this.onclick.emit(this.label);
 }
   constructor() { }
 
