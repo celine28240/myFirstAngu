5588d0c38e312355379803f09d56b183b8f6de0a
celine28240 <celinehenichard@gmail.com>
2019-04-01 17:00:15 +0200

    Correction erreur routes user

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,10 +8,7 @@ import {UserProfileComponent} from './user-profile/user-profile.component';
 const ROUTES : Routes = [
     {path : 'sign-up', component : SignUpComponent},
     {path : 'user-profile', component : UserProfileComponent},
-    {path : '',
-     redirectTo: '/user-profile',
-     pathMatch: 'full'
-    },
+    {path : '',component : UserProfileComponent},
 ];
 
 
